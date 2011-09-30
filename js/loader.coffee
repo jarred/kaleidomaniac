118653cba96ac6571685421e4483296513745bf6
Jarred <jarred@bitbybit.io>
2011-09-30 16:55:59 +1300

    Tidy.

--- js/loader.coffee
+++ js/loader.coffee
@@ -4,6 +4,7 @@ kelido.app = {}
 require ['jquery.min', 'underscore-min'], () ->
   if window.location.pathname is '/'
     window.location = $('.post').data('permalink')
+    return
   else
   	require ['backbone-min', 'views.cell', 'views.post', 'main'], () ->
   		kelido.app.init()
--- js/loader.js
+++ js/loader.js
@@ -4,7 +4,7 @@
   kelido.app = {};
   require(['jquery.min', 'underscore-min'], function() {
     if (window.location.pathname === '/') {
-      return window.location = $('.post').data('permalink');
+      window.location = $('.post').data('permalink');
     } else {
       require(['backbone-min', 'views.cell', 'views.post', 'main'], function() {
         kelido.app.init();

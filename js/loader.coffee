0f8e20242534b726f492fd09dcaa4a3be74d217a
Jarred <jarred@bitbybit.io>
2011-10-01 09:18:29 +1300

    Move loader.js code into theme.html for speed and development ease

--- js/loader.coffee
+++ js/loader.coffee
@@ -1,9 +1,14 @@
 kelido = window.Kelido ||= {}
 kelido.app = {}
 
+require.config
+  # baseUrl: 'http://localhost:4104/js/'
+  baseUrl: 'http://jarred.github.com/kaleidomaniac/js/'
+
 require ['jquery.min', 'underscore-min'], () ->
   if window.location.pathname is '/'
     window.location = $('.post').data('permalink')
+    return
   else
   	require ['backbone-min', 'views.cell', 'views.post', 'main'], () ->
   		kelido.app.init()
--- js/loader.js
+++ js/loader.js
@@ -2,9 +2,12 @@
   var kelido;
   kelido = window.Kelido || (window.Kelido = {});
   kelido.app = {};
+  require.config({
+    baseUrl: 'http://jarred.github.com/kaleidomaniac/js/'
+  });
   require(['jquery.min', 'underscore-min'], function() {
     if (window.location.pathname === '/') {
-      return window.location = $('.post').data('permalink');
+      window.location = $('.post').data('permalink');
     } else {
       require(['backbone-min', 'views.cell', 'views.post', 'main'], function() {
         kelido.app.init();
